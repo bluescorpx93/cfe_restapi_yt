86320e084e4ab0c43deebb2c63fd799da47f485d
Toufiq <burningraven06@gmail.com>
2018-01-27 22:33:53 +0600

    create model

--- postings/models.py
+++ postings/models.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class BlogPost(models.Model):
+	user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	title= models.CharField(max_length=120,null=True, blank=True)
+	content = models.TextField(max_length=120, null=True, blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.user
