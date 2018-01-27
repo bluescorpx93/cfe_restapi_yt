9499c77cf4b2a3fa20bda2e19c17f9d4df53ea7c
Toufiq <burningraven06@gmail.com>
2018-01-27 23:08:09 +0600

    create serializer to serialize data from db to json data

---
+++ postings/api/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from postings.models import BlogPost
+
+class BlogPostSerializers(serializers.ModelSerializer):
+	class Meta:
+		model = BlogPost
+		fields = [
+			'pk', 'user', 'title', 'content', 'timestamp'
+		]
