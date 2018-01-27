70cd1b6b5c7e3a7cb586ba2c065d07d6f3e8231a
Toufiq <burningraven06@gmail.com>
2018-01-28 00:15:21 +0600

    add create api view

--- postings/api/views.py
+++ postings/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, mixins
 from postings.models import BlogPost
 from .serializers import BlogPostSerializer
 
@@ -14,9 +14,22 @@ class BlogPostRUDView(generics.RetrieveUpdateDestroyAPIView):
 	# 	pk = self.kwargs.get("pk")
 	# 	return BlogPost.objects.get(pk=pk)
 
-class BlogPostAPIView(generics.CreateAPIView):
+class BlogPostAPIView(mixins.CreateModelsMixin, generics.ListAPIView):
 	lookup_field = 'pk'
 	serializer_class = BlogPostSerializer
 
 	def get_queryset(self):
 		return BlogPost.objects.all()
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user)
+
+	def post(self, request, *args, **kwargs):
+		return self.create(request, *args, **kwargs)
+
+	# def put(self, request, *args, **kwargs):
+	# 	return self.update(request, *args, **kwargs)
+
+	# def patch(self, request, *args, **kwargs):
+	# 	return self.update(request, *args, **kwargs)
+
