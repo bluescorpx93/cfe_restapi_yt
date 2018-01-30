1741a7ca1ee7c9086546d4632df68489c7cf980a
Toufiq <burningraven06@gmail.com>
2018-01-30 22:09:33 +0600

    include test file

---
+++ postings/api/tests.py
@@ -0,0 +1,19 @@
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from postings.models import BlogPost
+
+User = get_user_model()
+class BlogPostAPITestCase(APITestCase):
+	def setUp(self):
+		user_obj = User.objects.create(username='testusertoufiq', email='test@test.com')
+		user_obj.set_password('test1234')
+		user_obj.save()
+		blog_post = BlogPost.objects.create(user=user_obj, title='New Post 11', content='wow haha')
+		blog_post.save()
+
+	def test_single_user(self):
+		user_count = User.objects.count()
+		self.assertEqual(user_count, 1)
+
+
+
