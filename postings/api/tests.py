6f74d5815fe51cce6a084686798677ef7f1f17ad
Toufiq <burningraven06@gmail.com>
2018-01-30 23:12:24 +0600

    write some tests

--- postings/api/tests.py
+++ postings/api/tests.py
@@ -3,6 +3,11 @@ from rest_framework import status
 from django.contrib.auth import get_user_model
 from postings.models import BlogPost
 from rest_framework.reverse import reverse as api_reverse
+from rest_framework_jwt.settings import api_settings
+encode_handler = api_settings.JWT_PAYLOAD_HANDLER
+payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+
+
 
 User = get_user_model()
 class BlogPostAPITestCase(APITestCase):
@@ -40,14 +45,19 @@ class BlogPostAPITestCase(APITestCase):
 		response = self.client.get(url, data, format='json')
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-	def test_update_item(self):
+	def test_update_item_with_user(self):
 		blog_post = BlogPost.objects.first()
 		data={'title': "Something", 'content':'something called content'}
 		url = blog_post.get_api_url()
-		response = self.client.post(url, data, format='json')
-		self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+		user_obj = User.objects.first()
+		payload = payload_handler(user_obj)
+		token_rsp = encode_handler(payload)
+		self.client.credentials(HTTP_AUTHORIZATION='JWT' + token_rsp)
+
+		#response = self.client.post(url, data, format='json')
+		#self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 		response = self.client.put(url, data, format='json')
-		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
 
