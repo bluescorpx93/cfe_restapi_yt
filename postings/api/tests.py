d2564f7c42d9646f50d4f941f3dd4dd67420c499
Toufiq <burningraven06@gmail.com>
2018-01-30 22:58:35 +0600

    write tests

--- postings/api/tests.py
+++ postings/api/tests.py
@@ -1,7 +1,8 @@
 from rest_framework.test import APITestCase
+from rest_framework import status
 from django.contrib.auth import get_user_model
 from postings.models import BlogPost
-
+from rest_framework.reverse import reverse as api_reverse
 
 User = get_user_model()
 class BlogPostAPITestCase(APITestCase):
@@ -19,6 +20,34 @@ class BlogPostAPITestCase(APITestCase):
 	def test_single_post(self):
 		post_count = BlogPost.objects.count()
 		self.assertEqual(post_count, 1)
+
+	def test_get_list(self):
+		data = {}
+		url=api_reverse("api-postings:post-listcreate")
+		response = self.client.get(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_post_item(self):
+		data={'title': "Something", 'content':'something called content'}
+		url = api_reverse('api-postings:post-listcreate')
+		response = self.client.post(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+	def test_get_item(self):
+		blog_post = BlogPost.objects.first()
+		data = {}
+		url = api_reverse("api-postings:post-listcreate")
+		response = self.client.get(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+	def test_update_item(self):
+		blog_post = BlogPost.objects.first()
+		data={'title': "Something", 'content':'something called content'}
+		url = blog_post.get_api_url()
+		response = self.client.post(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+		response = self.client.put(url, data, format='json')
+		self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 
--- postings/api/urls.py
+++ postings/api/urls.py
@@ -2,6 +2,6 @@ from .views import BlogPostRUDView, BlogPostAPIView
 from django.conf.urls import url
 
 urlpatterns = [
-	url(r'^$', BlogPostAPIView.as_view(), name='post-create'),
+	url(r'^$', BlogPostAPIView.as_view(), name='post-listcreate'),
    url(r'^(?P<pk>\d+)/$', BlogPostRUDView.as_view(), name='post-rud'),
 ]
